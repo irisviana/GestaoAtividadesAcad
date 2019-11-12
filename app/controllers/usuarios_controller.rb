957a59b4d6e755382d6d516c497b067faa379598
Iris <vianasantana21@gmail.com>
2019-11-12 20:39:01 -0300

    configurando login com devise

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -69,6 +69,6 @@ class UsuariosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def usuario_params
-      params.require(:usuario).permit(:email, :senha, :nome, :cpf, :curso, :especificao)
+      params.require(:usuario).permit(:nome, :cpf, :curso, :especificao)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :usuarios, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   get 'welcome/index'
   resources :projetos do
     resources :trabalhos
     resources :bibliografia
     resources :atividades
   end
-  resources :usuarios
   resources :atividades do
     resources :historico_atividades
   end
